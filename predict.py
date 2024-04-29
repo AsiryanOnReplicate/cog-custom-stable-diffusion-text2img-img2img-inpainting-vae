b59545bf4c12510bfe3786a72b66f83ed1db08fa
asiryan <dmc5mod@yandex.ru>
2024-04-30 01:03:30 +0300

    Add new logic and schedulers

--- predict.py
+++ predict.py
@@ -1,16 +1,25 @@
 # Prediction interface for Cog ⚙️
+from typing import List
 from cog import BasePredictor, Input, Path
 import os
 import math
 import torch
 from PIL import Image
 from diffusers import AutoPipelineForText2Image, AutoPipelineForImage2Image, AutoPipelineForInpainting, UNet2DConditionModel, AutoencoderKL
-from diffusers import (DDIMScheduler,
+from diffusers import (
     DPMSolverMultistepScheduler,
-    EulerAncestralDiscreteScheduler,
+    DPMSolverSinglestepScheduler,
+    KDPM2DiscreteScheduler,
     EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
     HeunDiscreteScheduler,
-    PNDMScheduler
+    LMSDiscreteScheduler,
+    DDIMScheduler,
+    DEISMultistepScheduler,
+    UniPCMultistepScheduler,
+    LCMScheduler,
+    PNDMScheduler,
+    KDPM2AncestralDiscreteScheduler,
 )
 
 MODEL_CACHE = "cache"
@@ -18,30 +27,57 @@ MODEL_INPAINTING_CACHE = "inpainting_cache"
 VAE_CACHE = "vae-cache"
 
 SCHEDULERS = {
-    "DDIM": DDIMScheduler,
-    "DPMSolverMultistep": DPMSolverMultistepScheduler,
-    "HeunDiscrete": HeunDiscreteScheduler,
-    "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler,
-    "K_EULER": EulerDiscreteScheduler,
-    "PNDM": PNDMScheduler,
+    "Euler": (EulerDiscreteScheduler, {}),
+    "Euler Karras": (EulerDiscreteScheduler, {"use_karras_sigmas": True}),
+    "Euler A": (EulerAncestralDiscreteScheduler, {}),
+    "Euler A Karras": (EulerAncestralDiscreteScheduler, {"use_karras_sigmas": True}),
+
+    "Heun": (HeunDiscreteScheduler, {}),
+    "LMS": (LMSDiscreteScheduler, {}),
+    "LMS Karras": (LMSDiscreteScheduler, {"use_karras_sigmas": True}),
+
+    "DDIM": (DDIMScheduler, {}),
+    "DEIS": (DEISMultistepScheduler, {}),
+    "UniPC": (UniPCMultistepScheduler, {}),
+    "PNDM" : (PNDMScheduler, {}),
+
+    "DPM++ 2M": (DPMSolverMultistepScheduler, {}),
+    "DPM++ 2M Karras": (DPMSolverMultistepScheduler, {"use_karras_sigmas": True}),
+    "DPM++ 2M SDE": (DPMSolverMultistepScheduler, {"algorithm_type": "sde-dpmsolver++"}),
+    "DPM++ 2M SDE Karras": (DPMSolverMultistepScheduler, {"use_karras_sigmas": True, "algorithm_type": "sde-dpmsolver++"}),
+    "DPM++ SDE": (DPMSolverSinglestepScheduler, {}),
+    "DPM++ SDE Karras": (DPMSolverSinglestepScheduler, {"use_karras_sigmas": True}),
+
+    "DPM++ 2M Lu": (DPMSolverMultistepScheduler, {"use_lu_lambdas": True}),
+    "DPM++ 2M Ef": (DPMSolverMultistepScheduler, {"euler_at_final": True}),
+    "DPM++ 2M SDE Lu": (DPMSolverMultistepScheduler, {"use_lu_lambdas": True, "algorithm_type": "sde-dpmsolver++"}),
+    "DPM++ 2M SDE Ef": (DPMSolverMultistepScheduler, {"algorithm_type": "sde-dpmsolver++", "euler_at_final": True}),
+
+    "DPM2": (KDPM2DiscreteScheduler, {}),
+    "DPM2 Karras": (KDPM2DiscreteScheduler, {"use_karras_sigmas": True}),
+    "DPM2 A" : (KDPM2AncestralDiscreteScheduler, {}),
+    "DPM2 A Karras" : (KDPM2AncestralDiscreteScheduler, {"use_karras_sigmas": True}),
+
+    "LCM" : (LCMScheduler, {}),
 }
 
 class Predictor(BasePredictor):
     def setup(self):
+
         vae = AutoencoderKL.from_single_file(
-            "https://huggingface.co/asiryan/juggernaut/blob/main/TRCVAE.safetensors",
+            "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors",
             cache_dir=VAE_CACHE
         )
 
         self.text2img_pipe = AutoPipelineForText2Image.from_pretrained(
-            MODEL_CACHE,
+            MODEL_CACHE, 
             vae=vae,
             safety_checker = None, 
             custom_pipeline="lpw_stable_diffusion"
         ).to("cuda")
 
         self.img2img_pipe = AutoPipelineForImage2Image.from_pretrained(
-            MODEL_CACHE,
+            MODEL_CACHE, 
             vae=vae,
             safety_checker = None, 
             custom_pipeline="lpw_stable_diffusion"
@@ -62,11 +98,11 @@ class Predictor(BasePredictor):
         self,
         prompt: str = Input(
             description="Input prompt",
-            default="((masterpiece)),((best quality)),((high detial)),((realistic,)) Industrial age city, deep canyons in the middle, architectural streets, bazaars, Bridges, rainy days, steampunk, European architecture",
+            default="masterpiece, best quality, 1girl, bag, upper body, street, bangs, bare shoulders, beret, black footwear, black headwear, brown eyes, brown hair, closed mouth, handbag, hat, holding, long hair, long sleeves, looking at viewer, off shoulder, shoes, skirt, sleeves past wrists, socks, solo, sweater, white legwear",
         ),
         negative_prompt: str = Input(
             description="Input Negative Prompt",
-            default="(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
+            default="disfigured, kitsch, ugly, oversaturated, greain, low-res, deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, poorly drawn hands, missing limb, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, calligraphy, sign, writing, watermark, text, body out of frame, extra legs, extra arms, extra feet, out of frame, poorly drawn feet, cross-eye",
         ),
         image: Path = Input(
             description="Input image for img2img and inpainting modes",
@@ -87,6 +123,12 @@ class Predictor(BasePredictor):
             ge=0, 
             le=1920, 
             default=728
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output",
+            ge=1,
+            le=4,
+            default=1,
         ),
         strength: float = Input(
             description="Strength/weight", 
@@ -109,37 +151,32 @@ class Predictor(BasePredictor):
         scheduler: str = Input(
             description="Scheduler",
             choices=SCHEDULERS.keys(),
-            default="K_EULER_ANCESTRAL",
-        ),
-        use_karras_sigmas: bool = Input(
-            description="Use karras sigmas or not", 
-            default=False
+            default="Euler A Karras",
         ),
         seed: int = Input(
             description="Leave blank to randomize", 
             default=None
         ),
-    ) -> Path:
+    ) -> List[Path]:
         """Run a single prediction on the model"""
         if (seed == 0) or (seed == None):
             seed = int.from_bytes(os.urandom(2), byteorder='big')
         generator = torch.Generator('cuda').manual_seed(seed)
         
         print("Scheduler:", scheduler)
-        print("Using karras sigmas:", use_karras_sigmas)
-        print("Using seed:", seed)
+        print("Seed:", seed)
         
         if image and mask:
             print("Mode: inpainting")
             init_image = Image.open(image).convert('RGB')
             init_mask = Image.open(mask).convert('RGB')
             
-            self.inpainting_pipe.scheduler = SCHEDULERS[scheduler].from_config(
-                self.inpainting_pipe.scheduler.config, 
-                use_karras_sigmas=use_karras_sigmas)
+            scheduler = SCHEDULERS[scheduler]
+            self.inpainting_pipe.scheduler = scheduler[0].from_config(scheduler[1]).from_config(self.text2img_pipe.scheduler.config)
 
-            output_image = self.inpainting_pipe(
-                prompt=prompt,
+            output = self.inpainting_pipe(
+                prompt=[prompt] * num_outputs,
+                negative_prompt=[negative_prompt] * num_outputs,
                 image=init_image,
                 mask_image=init_mask,
                 strength=strength,
@@ -147,45 +184,46 @@ class Predictor(BasePredictor):
                 guidance_scale=guidance_scale,
                 width=self.base(width),
                 height=self.base(height),
-                negative_prompt=negative_prompt,
                 generator=generator,
-            ).images[0]
+            )
         elif image:
             print("Mode: img2img")
             init_image = Image.open(image).convert('RGB')
 
-            self.img2img_pipe.scheduler = SCHEDULERS[scheduler].from_config(
-                self.img2img_pipe.scheduler.config, 
-                use_karras_sigmas=use_karras_sigmas)
+            scheduler = SCHEDULERS[scheduler]
+            self.img2img_pipe.scheduler = scheduler[0].from_config(scheduler[1]).from_config(self.text2img_pipe.scheduler.config)
 
-            output_image = self.img2img_pipe(
-                prompt=prompt,
+            output = self.img2img_pipe(
+                prompt=[prompt] * num_outputs,
+                negative_prompt=[negative_prompt] * num_outputs,
                 image=init_image,
                 strength=strength,
                 num_inference_steps=num_inference_steps,
                 guidance_scale=guidance_scale,
                 width=self.base(width),
                 height=self.base(height),
-                negative_prompt=negative_prompt,
                 generator=generator,
-            ).images[0]
+            )
         else:
             print("Mode: text2img")
-            self.text2img_pipe.scheduler = SCHEDULERS[scheduler].from_config(
-                self.text2img_pipe.scheduler.config, 
-                use_karras_sigmas=use_karras_sigmas)
+            scheduler = SCHEDULERS[scheduler]
+            self.text2img_pipe.scheduler = scheduler[0].from_config(scheduler[1]).from_config(self.text2img_pipe.scheduler.config)
 
-            output_image = self.text2img_pipe(
-                prompt=prompt,
+            output = self.text2img_pipe(
+                prompt=[prompt] * num_outputs,
+                negative_prompt=[negative_prompt] * num_outputs,
                 strength=strength,
                 num_inference_steps=num_inference_steps,
                 guidance_scale=guidance_scale,
                 width=self.base(width),
                 height=self.base(height),
-                negative_prompt=negative_prompt,
-                generator=generator,
-            ).images[0]
+                generator=generator
+            )
         
-        out_path = Path(f"/tmp/output.png")
-        output_image.save(out_path)
-        return  out_path
+        output_paths = []
+        for i, image in enumerate(output.images):
+            output_path = f"/tmp/out-{i}.png"
+            image.save(output_path)
+            output_paths.append(Path(output_path))
+        
+        return output_paths
